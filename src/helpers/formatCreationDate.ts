80b3481ac7f6c0a12c833084757a9b54d449f039
emredurmayaz <emredurmayaz@hotmail.com>
2023-02-15 10:12:28 +0300

    Delete all files

--- src/helpers/formatCreationDate.ts
+++
@@ -1,9 +0,0 @@
-function formatCreationDate(date: string): string {
-	const dateObj = new Date(date || '');
-	const year = dateObj.getFullYear();
-	const month = dateObj.getMonth() + 1;
-	const day = dateObj.getDate();
-	return `${day}/${month}/${year}`;
-}
-
-export default formatCreationDate;
