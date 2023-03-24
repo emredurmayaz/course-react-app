43d312960e3446e4d1fed041d5cd5b0f4afc2c37
emredurmayaz <emredurmayaz@hotmail.com>
2023-03-24 14:33:00 +0300

    Axios render problem fixed

--- src/components/Courses/tests/courses.test.tsx
+++ src/components/Courses/tests/courses.test.tsx
@@ -3,19 +3,38 @@ import CourseCard from '../components/CourseCard/CourseCard';
 import { render, screen } from '@testing-library/react';
 
 // Courses should display amount of CourseCard equal length of courses array.Write test only jest and react-testing-library.
+// Courses › should display amount of CourseCard equal length of courses array
 describe('Courses', () => {
 	it('should display amount of CourseCard equal length of courses array', () => {
 		const courses = [
 			{
-				title: 'Eng555',
-				description: 'Eng555',
-				creationDate: '13/03/2023',
-				duration: 500,
-				authors: ['9b87e8b8-6ba5-40fc-a439-c4e30a373d36'],
+				id: '8eee6215-a175-43a5-8586-ed6aae81abb1',
+				title: 'Course 1',
+				description: 'Course 1 description',
+				creationDate: '2021-01-01',
+				duration: 120,
+				authors: ['9b87e8b8-6ba5-40fc-a439-c4e30a373d34'],
+			},
+			{
+				id: '8eee6215-a175-43a5-8586-ed6aae81abb2',
+				title: 'Course 2',
+				description: 'Course 2 description',
+				creationDate: '2021-01-01',
+				duration: 120,
+				authors: ['9b87e8b8-6ba5-40fc-a439-c4e30a373d35'],
+			},
+			{
 				id: '8eee6215-a175-43a5-8586-ed6aae81abb3',
+				title: 'Course 3',
+				description: 'Course 3 description',
+				creationDate: '2021-01-01',
+				duration: 120,
+				authors: ['9b87e8b8-6ba5-40fc-a439-c4e30a373d36'],
 			},
 		];
+
 		render(<CourseCard courses={courses} />);
-		expect(screen.getAllByTestId('course-card')).toHaveLength(1);
+
+		expect(screen.getAllByTestId('course-card')).toHaveLength(courses.length);
 	});
 });
